23e3b683fe35eec4c55e8285e49df708ca1a30d7
Jang Jaehue <jaehue@jang.io>
2016-11-22 10:45:44 +0800

    add mapping table

--- model/brand_customer.go
+++ model/brand_customer.go
@@ -3,11 +3,11 @@ package model
 import "time"
 
 type FashionBrandCustomer struct {
-	Id        int64
-	UserId    int64 `xorm:"index"`
-	BrandCode string
-	CustNo    string
-	WxOpenId  string
+	Id         int64
+	CustomerId int64 `xorm:"index"`
+	BrandCode  string
+	CustNo     string
+	WxOpenId   string
 
 	ReceiveAddress   string
 	ReceiveTelephone string
@@ -15,18 +15,28 @@ type FashionBrandCustomer struct {
 	ReceiveSize      string
 
 	Status    string    `xorm:"varchar(40)"`
-	CreatedAt time.Time `xorm:"created"`
-	UpdatedAt time.Time `xorm:"updated 'mode_date_time'"`
+	CreatedAt time.Time `xorm:"created 'in_date_time'"`
+	UpdatedAt time.Time `xorm:"updated 'modi_date_time'"`
 }
 
 type RetailBrandCustomer struct {
-	Id        int64
-	UserId    int64 `xorm:"index"`
-	BrandCode string
-	VipCode   string
-	WxOpenId  string
+	Id         int64
+	CustomerId int64 `xorm:"index"`
+	BrandCode  string
+	VipCode    string
+	WxOpenId   string
 
 	Status    string    `xorm:"varchar(40)"`
-	CreatedAt time.Time `xorm:"created"`
-	UpdatedAt time.Time `xorm:"updated 'mode_date_time'"`
+	CreatedAt time.Time `xorm:"created 'in_date_time'"`
+	UpdatedAt time.Time `xorm:"updated 'modi_date_time'"`
+}
+
+type FashionBrandCustomerInfo struct {
+	Customer             `xorm:"extends"`
+	FashionBrandCustomer `xorm:"extends"`
+}
+
+type RetailBrandCustomerInfo struct {
+	Customer            `xorm:"extends"`
+	RetailBrandCustomer `xorm:"extends"`
 }
--- model/customer.go
+++ model/customer.go
@@ -2,9 +2,27 @@ package model
 
 import "time"
 
-type Customer struct{
-        Id int64
-        Mobile string
-        CreatedAt time.Time
-        UpdatedAt time.Time
+type Customer struct {
+	Id        int64
+	Mobile    string    `xorm:"unique"`
+	CreatedAt time.Time `xorm:"created 'in_date_time'"`
+	UpdatedAt time.Time `xorm:"updated 'modi_date_time'"`
+}
+
+type CustomerInfo struct {
+	CustomerID    int64
+	Name          string
+	Mobile        string `xorm:"index"`
+	BrandCode     string
+	Gender        string
+	Birthday      string
+	Address       string
+	DetailAddress string
+	Email         string
+	IsMarried     bool
+	IsNewCust     int64
+	HasFilled     bool
+
+	CreatedAt time.Time `xorm:"created 'in_date_time'"`
+	UpdatedAt time.Time `xorm:"updated 'modi_date_time'"`
 }
